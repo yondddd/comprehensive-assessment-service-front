61b1193f4c92e8b0185136540cd18e92bb482fe8
wangjielong <wangjielong>
2024-02-18 00:49:00 +0800

    fix

--- src/views/personCenter/info/index.vue
+++ src/views/personCenter/info/index.vue
@@ -6,15 +6,16 @@ const userStore = useUserStore()
 
 <template>
   <div class="app-container">
-    <el-descriptions title="个人信息" direction="vertical" :column="4" border>
-      <el-descriptions-item label="学校">{{ userStore.orgName }}</el-descriptions-item>
-      <el-descriptions-item label="用户名">{{ userStore.username }}</el-descriptions-item>
-      <el-descriptions-item label="手机号">{{ userStore.mobile }}</el-descriptions-item>
-      <el-descriptions-item label="居住地" :span="2">苏州市</el-descriptions-item>
-      <el-descriptions-item label="备注">
-        <el-tag size="small">学校</el-tag>
+    <el-descriptions title="个人信息" direction="vertical" :column="2" border>
+      <el-descriptions-item label="姓名">{{ userStore.username }}</el-descriptions-item>
+      <el-descriptions-item label="角色">
+        <el-tag size="small">{{ userStore.orgName }}</el-tag>
       </el-descriptions-item>
-      <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
+      <el-descriptions-item label="学校">{{ userStore.orgName }}</el-descriptions-item>
+      <el-descriptions-item label="班级信息">{{ userStore.orgName }}</el-descriptions-item>
+      <el-descriptions-item label="学/工号">{{ userStore.username }}</el-descriptions-item>
+      <el-descriptions-item label="手机">{{ userStore.mobile }}</el-descriptions-item>
+      <el-descriptions-item label="邮箱">{{ userStore.email }}</el-descriptions-item>
     </el-descriptions>
   </div>
 </template>
